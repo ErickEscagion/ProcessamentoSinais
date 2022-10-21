a1ef029982171c4465665dbc98670b7937104a4f
erick <erickeescagion@gmail.com>
2022-10-21 20:06:40 -0300

    .

--- main_v2.py
+++ main_v2.py
@@ -20,8 +20,8 @@ from graph_to_img import generate_graph
 def main():
     # img_base = Image.open("imgs/praia.jpg")
     # img_base = Image.open("imgs/perfil.jpg")
-    img_base = Image.open("imgs/campo.jpg")
-    # img_base = Image.open("imgs/campoAgua.jpg")
+    # img_base = Image.open("imgs/campo.jpg")
+    img_base = Image.open("imgs/campoAgua.jpg")
     # img_base = Image.open("imgs/florestaVermelha.jpg")
     # img_base = create_image()
     width, height, dimensions, pixel_values = informations_to_image(img_base)
@@ -42,37 +42,24 @@ def main():
                 width, height, dimensions, calc_pixel_values(randon_algoritm(
                     width, height, dimensions, calc_pixel_values(img_base), 0, 20)), 70)), -2, 2)), 50)
 
-    generate_graph(img_blue, "graf_img_base")
+    generate_graph(img_base, "graf_img_base")
     generate_graph(img_blue, "graf_img_blue")
     generate_graph(img_red, "graf_img_red")
     generate_graph(img_green, "graf_img_green")
 
     r, g, b = calc_rgb(width, height, pixel_values)
+    generate_graph_results(abs(r - r), abs(g - g), abs(b - b), "results_base")
 
-    r_blue, g_blue, b_blue = calc_rgb(
-        width, height, calc_pixel_values(img_blue))
-    res_r_blue = r - r_blue
-    res_g_blue = g - g_blue
-    res_b_blue = b - b_blue
-    generate_graph_results(abs(res_r_blue), abs(
-        res_g_blue), abs(res_b_blue), "results_blue")
+    r_blue, g_blue, b_blue = calc_rgb(width, height, calc_pixel_values(img_blue))
+    generate_graph_results(abs(r - r_blue), abs(g - g_blue), abs(b - b_blue), "results_blue")
 
-    r_green, g_green, b_green = calc_rgb(
-        width, height, calc_pixel_values(img_green))
-    res_r_green = r - r_green
-    res_g_green = g - g_green
-    res_b_green = b - b_green
-    generate_graph_results(abs(res_r_green), abs(
-        res_g_green), abs(res_b_green), "results_green")
+    r_green, g_green, b_green = calc_rgb(width, height, calc_pixel_values(img_green))
+    generate_graph_results(abs(r - r_green), abs(g - g_green), abs(b - b_green), "results_green")
 
     r_red, g_red, b_red = calc_rgb(width, height, calc_pixel_values(img_red))
-    res_r_red = r - r_red
-    res_g_red = g - g_red
-    res_b_red = b - b_red
-    generate_graph_results(abs(res_r_red), abs(
-        res_g_red), abs(res_b_red), "results_red")
+    generate_graph_results(abs(r - r_red), abs(g - g_red), abs(b - b_red), "results_red")
 
-    f, axarr = plt.subplots(4, 2)
+    f, axarr = plt.subplots(4, 3)
 
     print("Obrigado por utilizar nosso algoritmo!")
     axarr[0, 0].imshow(img_base)
@@ -80,32 +67,48 @@ def main():
     axarr[0, 0].axis('off')
 
     axarr[0, 1].imshow(mpimg.imread("./graf_img_base.png"))
-    axarr[0, 1].title.set_text('Grafico da Figura Inicial')
+    axarr[0, 1].title.set_text('Pixels da Figura Inicial')
     axarr[0, 1].axis('off')
+
+    axarr[0, 2].imshow(mpimg.imread("./results_base.png"))
+    axarr[0, 2].title.set_text('Pixels da Figura Inicial - Pixels da Figura Inicial')
+    axarr[0, 2].axis('off')
 
     axarr[1, 0].imshow(img_red)
     axarr[1, 0].title.set_text('Figura Vermelha')
     axarr[1, 0].axis('off')
 
-    axarr[1, 1].imshow(mpimg.imread("./results_red.png"))
-    axarr[1, 1].title.set_text('Grafico da Figura Vermelha')
+    axarr[1, 1].imshow(mpimg.imread("./graf_img_red.png"))
+    axarr[1, 1].title.set_text('Pixels da Figura Vermelha')
     axarr[1, 1].axis('off')
+
+    axarr[1, 2].imshow(mpimg.imread("./results_red.png"))
+    axarr[1, 2].title.set_text('Pixels da Figura Inicial - Pixels da Figura Vermelha')
+    axarr[1, 2].axis('off')
 
     axarr[2, 0].imshow(img_blue)
     axarr[2, 0].title.set_text('Figura Azul')
     axarr[2, 0].axis('off')
 
-    axarr[2, 1].imshow(mpimg.imread("./results_blue.png"))
-    axarr[2, 1].title.set_text('Grafico da Figura Azul')
+    axarr[2, 1].imshow(mpimg.imread("./graf_img_blue.png"))
+    axarr[2, 1].title.set_text('Pixels da Figura Azul')
     axarr[2, 1].axis('off')
+
+    axarr[2, 2].imshow(mpimg.imread("./results_blue.png"))
+    axarr[2, 2].title.set_text('Pixels da Figura Inicial - Pixels da Figura Azul')
+    axarr[2, 2].axis('off')
 
     axarr[3, 0].imshow(img_green)
     axarr[3, 0].title.set_text('Figura Verde')
     axarr[3, 0].axis('off')
 
-    axarr[3, 1].imshow(mpimg.imread("./results_green.png"))
-    axarr[3, 1].title.set_text('Grafico da Figura Verde')
+    axarr[3, 1].imshow(mpimg.imread("./graf_img_green.png"))
+    axarr[3, 1].title.set_text('Pixels da Figura Verde')
     axarr[3, 1].axis('off')
+
+    axarr[3, 2].imshow(mpimg.imread("./results_green.png"))
+    axarr[3, 2].title.set_text('Pixels da Figura Inicial - Pixels da Figura Verde')
+    axarr[3, 2].axis('off')
     plt.show()
 
 
